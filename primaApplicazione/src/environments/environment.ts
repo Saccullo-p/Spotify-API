b3222c30f696dba42f03a18a79effb1dda8077c5
Saccullo-p <saccullo.pietro@iisgalvanimi.edu.it>
2022-09-30 10:00:26 +0000

    30.09

--- primaApplicazione/src/environments/environment.ts
+++ primaApplicazione/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  oauthToken: 'Bearer BQDzTcw3bZV-EQNJFg4LMCGkGTyZ-wnYn2KmLe09RTtM2j3W1D6cyhlVLrw94EIsrvmmp0q0Zwm8ItEC1hHWCZ4-f-ploT2YyLpfWb6Htue7Yv7YcBvPchZZHc1ri2ertIiOH4riwiSWX5FBpO5puADc1AbRopFGty_9Y0IafBeO-YeQ45ws8tlXuu2tuKBLC9k"'
+  oauthToken: 'Bearer BQCcq40M5iE-o-JMrtJRx9PcZjAS5FPjEDZBom-QbVHRKVAb5eGUgjjygtD95G3PRcICyk3KYI-8Szkx4QwVUuI4M_vSc-eclHtw4BYmnoPNUq481xZMAuQ8Fb2TRzPIE-aPWfjDMR_FXCl6fT-nkDiHbN-kieZb3Hbw___UEeVZEScjYne3HMTWS-nUwcS9YLY"'
 };
